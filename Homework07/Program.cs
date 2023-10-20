70eb206d6290cdb865f69b94ae4a9ed26c5c6f3a
Бекнур Саттар <www.asemush@gmail.com>
2023-10-20 21:25:41 +0600

    Добавьте файлы проекта.

---
+++ Homework07/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tanklib;
+
+namespace Homework07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] t34Tanks = new Tank[5];
+            Tank[] pantherTanks = new Tank[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                t34Tanks[i] = new Tank("T-34");
+                pantherTanks[i] = new Tank("Panther");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Battle {i + 1}: {t34Tanks[i].GetTankParameters()} vs {pantherTanks[i].GetTankParameters()}");
+
+                if (t34Tanks[i] ^ pantherTanks[i])
+                    Console.WriteLine("T-34 wins!");
+                else
+                    Console.WriteLine("Panther wins!");
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Tanklib/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tanklib
+{
+    public class Tank
+    {
+        private string tankName;
+        private int ammunitionLevel;
+        private int armorLevel;
+        private int maneuverabilityLevel;
+
+        public Tank(string name)
+        {
+            tankName = name;
+            Random random = new Random();
+            ammunitionLevel = random.Next(0, 101);
+            armorLevel = random.Next(0, 101);
+            maneuverabilityLevel = random.Next(0, 101);
+        }
+
+        public string GetTankParameters()
+        {
+            return $"{tankName}: Ammo {ammunitionLevel}%, Armor {armorLevel}%, Maneuverability {maneuverabilityLevel}%";
+        }
+
+        public static bool operator ^(Tank t1, Tank t2)
+        {
+            int parametersExceeded = 0;
+
+            if (t1.ammunitionLevel > t2.ammunitionLevel)
+                parametersExceeded++;
+
+            if (t1.armorLevel > t2.armorLevel)
+                parametersExceeded++;
+
+            if (t1.maneuverabilityLevel > t2.maneuverabilityLevel)
+                parametersExceeded++;
+
+            return parametersExceeded >= 2;
+        }
+    }
+}
